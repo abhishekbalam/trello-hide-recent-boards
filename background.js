f7c88e0281e657c3c480c5e8141d59d88c856173
abhishekbalam <abhishekbalam@gmail.com>
2018-12-05 12:25:28 +0530

    Wed Dec  5 12:25:23 IST 2018

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
     console.log('State Changed');
     const timer = setInterval(() => {
-        var test=chrome.tabs.executeScript(null,{code: 'var a=window.document.getElementsByClassName("boards-page-board-section")[1];if(a){try{a.parentNode.style.display="none";}catch(err){}}' });
+        var test=chrome.tabs.executeScript(null,{code: 'var a=window.document.getElementsByClassName("boards-page-board-section")[1];if(a){try{a.parentNode.style.display="none";}catch(err){}}' }, _=>chrome.runtime.lastError);
         if(test){
             clearInterval(timer);
         }
